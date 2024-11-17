d3312ee9d331c8de581e84b955d5d079699bf25e
HighNoonKieran <k2bunzai@gmail.com>
2024-11-17 15:04:06 -0700

    feat: edit button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 //dynamic year
-const fYear = document.getElementById("footerYear");
+const fYear = document.getElementById("footerY");
 const d = new Date(); 
 fYear.textContent = d.getFullYear();
+
+const greets = document.getElementById("welcome");
+ 
+if (d.getHours() < 12) //dispaly message according to time of day
+    greets.innerHTML = 'Good Morning,';
+else if (12 < d.getHours() <= 17) 
+    greets.innerHTML = 'Good Afternoon,';
+else
+    greets.innerHTML = 'Good Evening,';
+
+//adds a button that displays an alert
+    const btn = document.getElementById("btn-alert");
+btn.addEventListener("click", () => {
+    alert ('Please do not no that again!')
+});
+btn.addEventListener("mouseout", function(){
+    btn.textContent = "Don't Click Me"
+})
+btn.addEventListener("mouseover", function(){
+    this.textContent = "Wait, no. Stay away from me!"
+})
+
+for (let i = 1; i <= 12; i++) {
+    const li = document.createElement("li");
+   if (i % 2 === 0) {
+      li.textContent = " even";
+    }
+    else {
+      li.textContent = " odd";
+    }
+    document.getElementById(`loop`).appendChild(li);
+  }
+
